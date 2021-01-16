2fca2a234c11456126b70ebf5aaf5820d2d82e9d
Lavruk Yulia <lavruk_yulia@ukr.net>
2021-01-16 12:36:20 +0200

    apollo/client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, gql } from "@apollo/client";
+import "./App.css";
+
+const API_URL = "https://uwatch.live";
+const input = { type: "post", locale: "en", projectId: "1" };
+
+const GET_POST_LIST = gql`
+  query getPostList($input: PostSearchType) {
+    posts(input: $input, paging: { limit: 12 }) {
+      items {
+        id
+        type
+        locale
+        shortDescription
+        fullUrl
+        thumbnail
+        tags
+        title
+        publishedAt
+        __typename
+      }
+      __typename
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_POST_LIST, {
+    variables: { input },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.posts.items.map((item) => (
+        <div key={item.id}>
+          <img src={`${API_URL}${item.thumbnail}`} alt={item.title} />
+        </div>
+      ))}
     </div>
   );
 }
